4245192fc16d1f875a5b2b24dfb6bf1262b0d1c4
Daniel Evgrafov <danyaevgrafov@gmail.com>
2026-03-21 21:32:52 +0200

    adjust tailwind

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
-import './index.css'
+//import './index.css'
 import App from './app/App.tsx'
+import "./styles/globals.css";
+import "./styles/theme.css";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
